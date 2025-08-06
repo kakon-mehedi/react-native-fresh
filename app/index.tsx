beee3f9938d2fa3e9d1ec09c9ec863fa9e5eb559
kakon-selise <kakon.mehedi@selisegroup.com>
2025-08-06 12:57:08 +0600

    feat: Add Nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-green-600">Hello World Fresh</Text>
+    </View>
+  );
+}
