b70231b35e8b0fd5e3bb2c5c287362783973f523
kakon-selise <kakon.mehedi@selisegroup.com>
2025-08-06 15:30:32 +0600

    update

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-green-600">Hello World Fresh</Text>
+      <Text className="text-primary-400 text-5xl">Hello World Fresh</Text>
     </View>
   );
 }
